bc3924b6765aba3f181731dfdcdfdf35876b878d
BOMBARED12 <wildter1995@gmail.com>
2023-02-22 09:14:20 -0600

    initial commit

---
+++ app/src/main/java/com/adc/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.adc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.widget.Toast
+import com.adc.databinding.ActivityMainBinding
+import kotlin.math.log
+
+class MainActivity :AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnShowToast.setOnClickListener {
+            Log.i("MainActivity","Button was clicked")
+            Toast.makeText(this,"Button was clicked!",Toast.LENGTH_LONG).show()
+        }
+    }
+}
